1ea5787aaeac593e31af476f0e3263bf75f8756b
JaneA <janejira885@gmail.com>
2020-04-11 15:44:07 +0700

    back: edit chat display

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -1,28 +1,45 @@
 import React from 'react';
+import NavBar from './navBar'
+import { Button, TextField } from '@material-ui/core';
 import './../style/chat.css'
+import eggie1 from './../asset/eggie1.png'
 
 import NavBar from './navBar'
 import Drawer from './drawer'
 
 class Chat extends React.Component {
+    state = {
+        username: 'Yinza55+',
+        group: 'Ekkie',
+        profile: eggie1,
+    }
     render() {
-
-
         return (
             <div className='chat' >
                 {/* <NavBar></NavBar> */}
                 <Drawer></Drawer>
                 <div className='chat-panel'>
                     <div className='chat-username'>
-                        Chat username
-            </div>
+                        <img src={this.state.profile}></img>
+                        <p>{this.state.username}</p>
+                        <span> | </span>
+                        <Button className='underline' color="inherit">Log out</Button>
+                    </div>
                     <div className='chat-group-name'>
-                        chat group name
-                </div>
-                    <div className='display-chat-messages'>
+                        {this.state.group}
+                    </div>
+                    <div className='chat-content'>
                         chat messages
-                </div>
-                    <div>typping</div>
+                    </div>
+                    <div className='message-box'>
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            placeholder='Type your message'
+                            fullWidth='true'>
+                        </TextField>
+                        <Button>Send</Button>
+                    </div>
                 </div>
             </div>
         )
