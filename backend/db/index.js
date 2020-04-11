0f343ffc1a63877bc89b017ca8b9d6c1ada3e5bb
littlemill <millmill.romnalin@hotmail.com>
2020-04-11 16:20:14 +0700

    commented code related to initial mock-up data

--- backend/db/index.js
+++ backend/db/index.js
@@ -17,42 +17,43 @@ const db = mongoose.connection
 
 //mock-data 
 
-var mill = new User({name: 'littlemill'})
-var yin = new User({name: 'yin_kiatsilp'})
-var nut = new User({name: 'nnnnnnutt'})
-var tien = new User({name: 'xntien'})
-var jane = new User({name: 'janejira_jira'})
+// var mill = new User({name: 'littlemill'})
+// var yin = new User({name: 'yin_kiatsilp'})
+// var nut = new User({name: 'nnnnnnutt'})
+// var tien = new User({name: 'xntien'})
+// var jane = new User({name: 'janejira_jira'})
 
-mill.save()
-yin.save()
-nut.save()
-tien.save()
-jane.save()
+// mill.save()
+// yin.save()
+// nut.save()
+// tien.save()
+// jane.save()
 
-var group1 = new Group({name: 'konsuaysuay'})
-var group2 = new Group({name: 'parallel-dist'})
-var group3 = new Group({name: 'ComEngCp44'})
+// var group1 = new Group({name: 'konsuaysuay'})
+// var group2 = new Group({name: 'parallel-dist'})
+// var group3 = new Group({name: 'ComEngCp44'})
 
-group1.save()
-group2.save()
-group3.save()
+// group1.save()
+// group2.save()
+// group3.save()
 
-new GroupMember({group: 'konsuaysuay',member: 'littlemill'}).save()
-new GroupMember({group: 'konsuaysuay',member: 'yin_kiatsilp'}).save()
-new GroupMember({group: 'konsuaysuay',member: 'nnnnnnutt'}).save()
-new GroupMember({group: 'konsuaysuay',member: 'xntien'}).save()
-new GroupMember({group: 'konsuaysuay',member: 'janejira_jira'}).save()
+// new GroupMember({group: 'konsuaysuay',member: 'littlemill'}).save()
+// new GroupMember({group: 'konsuaysuay',member: 'yin_kiatsilp'}).save()
+// new GroupMember({group: 'konsuaysuay',member: 'nnnnnnutt'}).save()
+// new GroupMember({group: 'konsuaysuay',member: 'xntien'}).save()
+// new GroupMember({group: 'konsuaysuay',member: 'janejira_jira'}).save()
 
-new GroupMember({group: 'parallel-dist',member: 'littlemill'}).save()
-new GroupMember({group: 'parallel-dist',member: 'yin_kiatsilp'}).save()
+// new GroupMember({group: 'parallel-dist',member: 'littlemill'}).save()
+// new GroupMember({group: 'parallel-dist',member: 'yin_kiatsilp'}).save()
 
-new GroupMember({group: 'ComEngCp44',member: 'nnnnnnutt'}).save()
-new GroupMember({group: 'ComEngCp44',member: 'xntien'}).save()
-new GroupMember({group: 'ComEngCp44',member: 'janejira_jira'}).save()
+// new GroupMember({group: 'ComEngCp44',member: 'nnnnnnutt'}).save()
+// new GroupMember({group: 'ComEngCp44',member: 'xntien'}).save()
+// new GroupMember({group: 'ComEngCp44',member: 'janejira_jira'}).save()
 
 // new Chat({    user: 'littlemill' ,group: 'konsuaysuay',timestamp: new Date('April 11, 2020 11:11:00'),message: 'hi beautiess ! xoxo'}).save()
 
 // var query = Group.find()
 // query.then(function(group))
+
 
 module.exports = db
